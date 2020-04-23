a8b8d3ba8e0ad86118583f05c326c1220d811106
Dario Ferrer <dario@mayara.io>
2020-04-23 19:59:28 +0200

    Debug Mode: Taken off
    The debug is done now using native terraform ways.

--- tf_provider/main.go
+++ tf_provider/main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "gitlab.com/mayara/private/anthos/hub"
-    "gitlab.com/mayara/private/anthos/k8s"
     "github.com/hashicorp/terraform-plugin-sdk/plugin"
     "github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-// DebugMode is used only for development purposes, if set to true, then debug() gets executed instead of main()
-const DebugMode = true
-
 func main() {
-    if DebugMode {
-        debug()
-    } else {
-        plugin.Serve(&plugin.ServeOpts{
-            ProviderFunc: func() terraform.ResourceProvider {
-                return Provider()
-            },
-        })
-    }
-}
-
-func debug() {
-    //fmt.Println("Calling get function:")
-    //err := hub.GetMembership("mayara-anthos", "mayara-fake", "", "", "", "", )
-    fmt.Println("Calling create function:")
-    err := hub.CreateMembership("mayara-anthos", "mayara-fake", "fake_description", "", "814a82ce-80da-4ad9-b6b4-30aaaaaa7777", "")
-    if err != nil{
-        println("Error: ", err.Error())
-    }
+    plugin.Serve(&plugin.ServeOpts{
+        ProviderFunc: func() terraform.ResourceProvider {
+            return Provider()
+        },
+    })
 }
