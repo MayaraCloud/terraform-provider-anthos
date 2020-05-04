75b27954e692f0dce8d55f5bc9eec7a9fc9e0f14
Dario Ferrer <dario@mayara.io>
2020-05-04 13:51:52 +0200

    Example: example file moved
    It was not clear if it was an example or a needed file.

--- example.tf
+++ example.tf
@@ -1,3 +1,5 @@
+# This is just an example file
+
 resource "anthos_cluster_membership" "mayara_eks" {
     cluster_name = "mayara-eks"
     k8s_context = "mayara-eks"
@@ -11,10 +13,3 @@ resource "anthos_gke_connect_agent" "mayara_eks_agent" {
     description = "mayara AWS based k8s gke connect agent"
     project = anthos_cluster_membership.mayara_eks.hub_project_id
 }
-
-#resource "anthos_cluster_membership" "mayara_gke" {
-#    cluster_name = "mayara-gke"
-#    k8s_context = "mayara-gke"
-#    description = "mayara GKE based k8s cluster"
-#    hub_project_id = "mayara-anthos"
-#}
